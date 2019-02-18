64127e703020c838538c2cec9a96d8f9ad0df6c8
Zui Dighe <zdighe@Zuis-MacBook-Pro.local>
2019-02-17 21:40:59 -0500

    changing index root

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var port = process.env.PORT || 8080;
 server.use(express.static(__dirname+'/public'));
 
 server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname});
+  response.sendFile('public/html/home-portfolio-minimal.html', {root: __dirname});
 });
 
 server.get('/#about', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname});
+  response.sendFile('public/html/home-portfolio-minimal.html', {root: __dirname});
 });
 
 server.listen(port, function(){
